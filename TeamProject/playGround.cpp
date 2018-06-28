6c45c7a7e313e21680eaf9010c55662711d9a9c4
maple9420 <maple9420@naver.com>
2018-06-28 10:24:17 +0900

    test 프로젝트

---
+++ TeamProject/playGround.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "playGround.h"
+
+
+playGround::playGround()
+{
+}
+
+
+playGround::~playGround()
+{
+
+}
+
+HRESULT playGround::init(void)	
+{
+	gameNode::init(true);
+
+	
+
+	return S_OK;
+}
+
+void playGround::release(void)	
+{
+	gameNode::release();
+
+	
+}
+
+void playGround::update(void)	
+{
+	gameNode::update();
+
+
+}
+
+void playGround::render(void)
+{
+	
+	//Èò»öµµÈ­Áö ÇÑ Àå ±ò¾ÆµÐ´Ù
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, BLACKNESS);
+	
+	
+	
+
+
+
+	TIMEMANAGER->render(getMemDC());
+	this->getBackBuffer()->render(getHDC(), 0, 0);
+
+}
+
---
+++ TeamProject/playGround.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "gameNode.h"
+
+class playGround : public gameNode
+{
+private:
+	
+
+public:
+	virtual HRESULT init(void);
+	virtual void release(void);
+	virtual void update(void);
+	virtual void render(void);
+
+	playGround();
+	~playGround();
+};
+
