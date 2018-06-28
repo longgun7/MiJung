9a2cbfde11a0a1a5e9d6a477db17bf72286e40e1
장동찬 <wkdehdcks12@naver.com>
2018-06-28 17:03:43 +0900

    stage3 > skeleton.cpp, h 작성 boss > blackUniform.cpp, h, 추가 boss.cpp, h 추가

---
+++ TeamProject/blackUniform.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "blackUniform.h"
+
+
+HRESULT blackUniform::init()
+{
+	return S_OK;
+}
+
+void blackUniform::release()
+{
+}
+
+void blackUniform::update()
+{
+}
+
+void blackUniform::render()
+{
+}
+
+blackUniform::blackUniform()
+{
+}
+
+
+blackUniform::~blackUniform()
+{
+}
