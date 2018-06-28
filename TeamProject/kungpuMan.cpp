987ad8afbe2011d4a35d17dcb1cc4fb92bee755e
장동찬 <wkdehdcks12@naver.com>
2018-06-28 12:18:47 +0900

    스테이지1,2,3 폴더 추가, 스테이지1 적 3마리 클래스 헤더 추가

---
+++ TeamProject/kungpuMan.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "kungpuMan.h"
+
+
+HRESULT kungpuMan::init()
+{
+	return S_OK;
+}
+
+void kungpuMan::release()
+{
+}
+
+void kungpuMan::update()
+{
+}
+
+void kungpuMan::render()
+{
+}
+
+kungpuMan::kungpuMan()
+{
+}
+
+
+kungpuMan::~kungpuMan()
+{
+}
---
+++ TeamProject/spearMan.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "enemy.h"
+class spearMan :
+	public enemy
+{
+public:
+	spearMan();
+	~spearMan();
+};
+
