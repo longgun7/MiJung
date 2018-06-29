3a7945c4660632538195ff5f611ed1d750e56d11
이승환 <tmdghks7836@naver.com>
2018-06-29 16:26:25 +0900

    아이템 매니저 오류 뜬거 수정

--- TeamProject/itemManager.cpp
+++ TeamProject/itemManager.cpp
@@ -16,20 +16,18 @@ HRESULT itemManager::init()
 	_weapon = new item;
 	_weapon->init(8);
 
-	for (int i = 0; i < 4; ++i)
-	{
-		_weapon->makeItem(i, 0, 100 + 100 * i, 100);	
-		_weapon->makeItem(i, 2, 500 + 100 * i, 100);
-	}
-
 	_potion = new item;
 	_potion->init(1);
-
-	_potion->makepotion(2, 3, 100, 200);
-		(*weapone).makeItem(i, 0, 100 + 100 * i, 100);
-	
+	for (int i = 0; i < 8; ++i)
+	{
+		_weapon->makeItem(i, 0, 100 + 100 * i, 100);	
 		
-	}*/
+	}
+
+	_potion->makepotion(0, 3, 100, 200);
+
+	
+
 	
 
 	return S_OK;
