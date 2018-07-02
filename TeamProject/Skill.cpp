33b2d9b58539293f55c7296ccb72640c038bbf7a
longgun7 <soundmusic64@gmail.com>
2018-07-02 10:25:32 +0900

    Revert "skill.class 호격권 클래스 수정 및 추가"
    This reverts commit e750b4ffa52f2eecc2be10ffa1e2427866ec750f.

--- TeamProject/Skill.cpp
+++ TeamProject/Skill.cpp
@@ -1,34 +1,27 @@
 #include "stdafx.h"
 #include "Skill.h"
 
-targetSkill1::targetSkill1(){}
+Skill::Skill(){}
 
-targetSkill1::~targetSkill1(){}
+Skill::~Skill(){}
 
-HRESULT targetSkill1::init()
+HRESULT Skill::init()
 {
 	IMAGEMANAGER->addFrameImage("TargetSkill1", "TargetSkill1.bmp", 100, 85, 1, 1, true, RGB(255, 0, 255), true);
-
-	_alphaColor = 0;
-	_alphaCount = 0;
-	_currentExp = 0;
-	_maxExp = 100;
-	_level = 1;
 
 	return S_OK;
 }
 
-void targetSkill1::realse()
+void Skill::realse()
 {
 }
 
-void targetSkill1::update()
+void Skill::update()
 {
 	for (_viTagSkill = _vTagSkill.begin(); _viTagSkill != _vTagSkill.end(); ++_viTagSkill)
 	{
 		_viTagSkill->count++;
-		// 스킬 2레벨 이상부터 시작
-		if (_level > 1)
+		if (_viTagSkill->level > 1)
 		{
 			if (_alphaMaxCount <= _alphaCount)
 			{
@@ -44,8 +37,8 @@ void targetSkill1::update()
 				_alphaCount++;
 			}
 		}
-		//카운터가 300이 되면 스킬 사라짐
-		if (_viTagSkill->count % 200 == 0)
+
+		if (_viTagSkill->count % 300 == 0)
 		{
 			_viTagSkill->count = 0;
 			_alphaCount = 0;
@@ -57,58 +50,53 @@ void targetSkill1::update()
 	}
 }
 
-void targetSkill1::render()
+void Skill::render()
 {
 	for (_viTagSkill = _vTagSkill.begin(); _viTagSkill != _vTagSkill.end(); ++_viTagSkill)
 	{
-		// 알파 프레임 랜더
 		_viTagSkill->img->alphaFrameRender(getMemDC(), _viTagSkill->rc.left, _viTagSkill->rc.top,
 			_viTagSkill->img->getFrameX(), _viTagSkill->img->getFrameY(), _alphaColor);
-
-	//	char str[128];
-	//	sprintf_s(str, "%d, %d, %d", _viTagSkill->count, _viTagSkill->x, _viTagSkill->x);
-	//	TextOut(getMemDC(), 200, 200, str, strlen(str));
 	}
+
 }
 
-void targetSkill1::addSkill(float x, float y)
+void Skill::addSkill(float x, float y)
 {
 	tagSkill targetSkill1;
 	ZeroMemory(&targetSkill1, sizeof(tagSkill));
 
-	targetSkill1.img = IMAGEMANAGER->findImage("TargetSkill1");		// 호격권 불러
-	targetSkill1.x = x;				// x 좌표
-	targetSkill1.y = y;				// y 좌표
-	targetSkill1.count = 0;			// 스킬 생성시 카운터 0 초기화
-	_currentExp += 40;				// 스킬 생성시 경험치 40 증가
+	targetSkill1.img = IMAGEMANAGER->findImage("TargetSkill1");
+	targetSkill1.x = x;
+	targetSkill1.y = y;
+	targetSkill1.count = 0;
+	targetSkill1.currentExp += 40;
+	targetSkill1.maxExp = 100;
 
-	// 현재 스킬 경험치가 최대치 보다 높으면 레벨업
-	if (_currentExp >= _maxExp)
+	if (targetSkill1.currentExp >= targetSkill1.maxExp)
 	{
-		_currentExp = 0;
-		_level++;
-		if (_level > 4)
+		targetSkill1.currentExp = 0;
+		targetSkill1.level++;
+		if (targetSkill1.level > 4)
 		{
-			_level = 4;
+			targetSkill1.level = 4;
 		}
 	}
 
-	// 각 레벨에 따라 알파 최대 카운터 변경
-	if (_level == 1)
+	if (targetSkill1.level == 1)
 	{
 		_alphaMaxCount = 2;
 	}
-	else if (_level == 2)
+	else if (targetSkill1.level == 2)
 	{
-		_alphaMaxCount = 3;
+		_alphaMaxCount = 2;
 	}
-	else if (_level == 3)
+	else if (targetSkill1.level == 3)
 	{
-		_alphaMaxCount = 5;
+		_alphaMaxCount = 2;
 	}
-	else if (_level == 4)
+	else if (targetSkill1.level == 4)
 	{
-		_alphaMaxCount = 7;
+		_alphaMaxCount = 2;
 	}
 	targetSkill1.rc = RectMakeCenter(targetSkill1.x, targetSkill1.y, targetSkill1.img->getFrameWidth(), targetSkill1.img->getFrameHeight());
 
--- TeamProject/Skill.h
+++ TeamProject/Skill.h
@@ -4,59 +4,29 @@
 
 struct tagSkill
 {
-	image* img;			// 스킬 이펙트 이미지
-	RECT rc;			// 스킬 상자
-	float x, y;			// 스킬 좌표
-	bool start;			// 스킬 시작
-	int count;			// 스킬 카운트
+	image* img;
+	RECT rc;
+	float x, y;
+	int currentExp = 0;
+	int maxExp;
+	bool start;
+	int level = 1;
+	int count;
 };
 
-class targetSkill1 : public gameNode
+class Skill : public gameNode
 {
 private:
 	vector<tagSkill>			_vTagSkill;
 	vector<tagSkill>::iterator	_viTagSkill;
 
-	int _currentExp;	// 스킬 현재 경험치
-	int _maxExp;		// 스킬 최대 경험치
-	int _level;			// 스킬 레벨
-
-	int _alphaColor;	// 스킬 알파 색
-	int _alphaCount;	// 스킬 알파 카운트
-	int _alphaMaxCount;	// 스킬 알파 최대 카운트
-
+	int _alphaColor;
+	int _alphaCount;
+	int _alphaMaxCount;
+	 
 public:
-	targetSkill1();
-	~targetSkill1();
-
-	HRESULT init();
-	void realse();
-	void update();
-	void render();
-
-	void addSkill(float x, float y, int dmage);
-
-	vector<tagSkill>getVTagSkill() { return _vTagSkill; }
-	vector<tagSkill>::iterator getVITagSkill() { return _viTagSkill; }
-};
-
-class targetSkill2 : public gameNode
-{
-private:
-	vector<tagSkill>			_vTagSkill;
-	vector<tagSkill>::iterator	_viTagSkill;
-
-	int _currentExp;	// 스킬 현재 경험치
-	int _maxExp;		// 스킬 최대 경험치
-	int _level;			// 스킬 레벨
-
-	int _alphaColor;	// 스킬 알파 색
-	int _alphaCount;	// 스킬 알파 카운트
-	int _alphaMaxCount;	// 스킬 알파 최대 카운트
-
-public:
-	targetSkill2();
-	~targetSkill2();
+	Skill();
+	~Skill();
 
 	HRESULT init();
 	void realse();
@@ -64,7 +34,5 @@ public:
 	void render();
 
 	void addSkill(float x, float y);
-
-	vector<tagSkill>getVTagSkill() { return _vTagSkill; }
-	vector<tagSkill>::iterator getVITagSkill() { return _viTagSkill; }
 };
+
