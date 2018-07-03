d763f4b9a9238a341f870bb944bea7be79b28639
승환 이 <tmdghks7836@naver.com>
2018-07-03 19:19:30 +0900

    playerGround 전방선언 지움

--- TeamProject/playGround.cpp
+++ TeamProject/playGround.cpp
@@ -29,12 +29,11 @@ HRESULT playGround::init(void)
 	_em->init();
 
 	//아타호와 에너미 매니저 전방선언
-	_em->setPlayerAddressLink(_pm->getPlayer());
+	
 	SCENEMANAGER->changeScene("스타트씬");
 	
 	//전방선언
 	_pm->itemManagerAdressLink(_im);
-	_pm->getPlayer()->EnemyManagerAdressLink(_em);
 	return S_OK;
 }
 
