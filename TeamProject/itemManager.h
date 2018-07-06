bff7ad599794e8668de539f3519e15b6b3816eaf
태환 김 <ik696393@naver.com>
2018-07-06 14:38:27 +0900

    플레이어 아이템을 하나로 묶었습니다 그리고 아이템 이름으로 스마슈와 야타호 이펙트가 나오도록 하였습니다.

--- TeamProject/itemManager.h
+++ TeamProject/itemManager.h
@@ -27,13 +27,14 @@ public:
 
 	void itemMakeSet(string itemName, float x, float y);
 
-	item* getA_Weapon() { return _a_Weapon; }
-	item* getA_Armor() { return _a_Armor; }
-	item* getS_Weapon() { return _s_Weapon; }
-	item* getS_Armor() { return _s_Armor; }
+	//item* getPItem() { return _a_Weapon; }
+	//item* getPItem() { return _a_Armor; }
+	//item* getPItem() { return _s_Weapon; }
+	//item* getPItem() { return _s_Armor; }
 	item* getPortion() { return _potion; }
 
 	item* getPItem() { return _pItem; }
 	item* getShopItem() { return _shopItem; }
+
 };
 
