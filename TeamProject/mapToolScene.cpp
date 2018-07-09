3cce48d1ce58d7d8bf5ef28bebf5573baa5bc2c9
longgun7 <soundmusic64@gmail.com>
2018-07-09 16:11:17 +0900

    필드1, 필드2 맵 그림

--- TeamProject/mapToolScene.cpp
+++ TeamProject/mapToolScene.cpp
@@ -119,7 +119,7 @@ void mapToolScene::render(void)
 				SetTextColor(getMemDC(), RGB(0, 200, 200));
 				SetBkMode(getMemDC(), TRANSPARENT);
 				char str[128];
-				sprintf_s(str, "%d", _tiles[i * TILEX + j].terrain); // i * TILEX + j);
+				sprintf_s(str, "%d", (i * TILEX + j) % 1000); // i * TILEX + j);
 				TextOut(getMemDC(), _tiles[i * TILEX + j].rc.left + (_tiles[i * TILEX + j].rc.right - _tiles[i * TILEX + j].rc.left) / 2 - 10
 					, _tiles[i * TILEX + j].rc.top + (_tiles[i * TILEX + j].rc.bottom - _tiles[i * TILEX + j].rc.top) / 2 - 10
 					, str, strlen(str));
