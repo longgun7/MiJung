9e7e5e74dc0b0e8420c9fda43ab6d181ffcb281a
홍민경 <hmin7082@gmail.com>
2018-07-13 11:38:12 +0900

    필드씬 3 들어가면 느려지는 문제 수정

--- TeamProject/barScnen.cpp
+++ TeamProject/barScnen.cpp
@@ -264,7 +264,7 @@ void barScnen::sceneChange(void)
 	case OBJ_DOWNPORTAL:
 		_pm->getPlayer()->setX(550); _pm->getPlayer()->setY(250);
 		_pm->getPlayer2()->setX(550); _pm->getPlayer2()->setY(250);
-		SCENEMANAGER->changeScene("ÀÌº¥Æ®¾À");
+		SCENEMANAGER->changeScene("ÇÊµå¾À3");
 		break;
 
 	case OBJ_LEFTPORTAL:	break;
