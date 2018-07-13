1485ae06326ba255dc4a8eecedb1d7e741d84cb3
longgun7 <soundmusic64@gmail.com>
2018-07-13 11:21:10 +0900

    커밋

--- TeamProject/barScnen.cpp
+++ TeamProject/barScnen.cpp
@@ -263,7 +263,7 @@ void barScnen::sceneChange(void)
 	case OBJ_DOWNPORTAL:
 		_pm->getPlayer()->setX(550); _pm->getPlayer()->setY(250);
 		_pm->getPlayer2()->setX(550); _pm->getPlayer2()->setY(250);
-		SCENEMANAGER->changeScene("Å¸¿î¾À");
+		SCENEMANAGER->changeScene("ÀÌº¥Æ®¾À");
 		break;
 
 	case OBJ_LEFTPORTAL:	break;
